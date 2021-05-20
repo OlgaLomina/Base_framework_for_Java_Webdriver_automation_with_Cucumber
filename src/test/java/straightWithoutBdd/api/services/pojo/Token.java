672616b0b4eedf5f288c2758f86ee734a0b678e3
vitaliifokine@gmail.com <vf281992>
2021-05-20 19:29:15 -0400

    Adjusted Code

---
+++ src/test/java/straightWithoutBdd/api/services/pojo/Token.java
@@ -0,0 +1,20 @@
+package straightWithoutBdd.api.services.pojo;
+
+public class Token {
+    private String token;
+
+    Token() {
+    }
+
+    public Token(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
--- src/test/java/straightWithoutBdd/utils/DriverInitializer.java
+++ src/test/java/straightWithoutBdd/utils/DriverInitializer.java
@@ -1,4 +1,4 @@
-package straightWithoutBdd.ui.utils;
+package straightWithoutBdd.utils;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
