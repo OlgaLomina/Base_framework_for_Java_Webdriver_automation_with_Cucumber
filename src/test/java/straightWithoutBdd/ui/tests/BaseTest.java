0282ac8a8c732ec09825331aaf379b42ab35ffe8
mirabel2015 <mirabel.2015m@gmail.com>
2021-06-10 10:43:19 -0700

    add changes from code review

--- src/test/java/straightWithoutBdd/ui/tests/BaseTest.java
+++ src/test/java/straightWithoutBdd/ui/tests/BaseTest.java
@@ -2,8 +2,11 @@ package straightWithoutBdd.ui.tests;
 
 import bdd.support.Loggable;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 import static straightWithoutBdd.utils.DriverInitializer.setBrowser;
 
@@ -13,11 +16,20 @@ public class BaseTest implements Loggable {
     @BeforeTest
     public void setup() {
         driver = setBrowser("chrome");
-        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        if (driver != null) {
+            driver.manage().window().maximize();
+        }
     }
 
-    @Test
-    public void testSample() {
-        driver.get("https://www.google.com/");
+//    @Test
+//    public void testSample() {
+//        driver.get("https://www.google.com/");
+//    }
+
+    @AfterTest
+    public void closeDriver(){
+        driver.quit();
     }
+
 }
