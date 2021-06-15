0db2124b7cd56736f56a87e0e5eaed11ee1ff4cc
ayou-jan <hairartur@gmail.com>
2021-06-14 21:02:13 -0400

    Task 3 - new Commit

---
+++ src/test/java/codingTasks/JavaTasks.java
@@ -0,0 +1,98 @@
+package codingTasks;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class JavaTasks {
+    public static void main(String[] args) {
+        //System.out.println(countConsistentStrings("abc", new String[]{"a","b","c","ab","ac","bc","abc"}));
+        //System.out.println(removeDuplicates(new int[]{0,0,1,1,1,2,2,3,3,4}));
+        System.out.println(validPalindrome("uvmthjvgjwhbtgairqxfagviertrvnqsmiohthvtpiykdyezewcekbznxyzwekrgnstwgdgaopvsgttntwvfaeorhmsstcilpiyjiutsiwwqeqgscxpcwfkforowqqvduassrzrjeicmfvfhfvjayupewgwzspwbnhjnaworvydvxefezxnerufgzsoymuyaaygccqirlzjvyusvbbotsmlxycoftedddtletrsrydxgeuymlmmcmfxwuwiazrgcybvbvidyoozvcekobwkkqoomeorcbxlzqydmmtmhvaozphkphfqdpvsycpzyoaysvfwmceynifnptmdzmxdfhjgykuzixkgxaguhvlsieaztozpzhvolywyrepqgrfpioausydostncbifqxambskirvcvdrzycxzibylyjqhttvcswjzbxasyscpikoydhvzuvyenioqxdgjqwdnpwamuldkbmvqfifoqzcrakzajkoybnblyakbnoaoolhjrjqybzqbitenhezrkxcbpvdxriwxdhmnelgbzkkpzbkkbeogkmihnreuxoepuvcrohunscztpuuzzxgfqohyyqgpfvhqtjlplvttintopkmdxzijxjzeirgmlfydilsqrhnpnvndtpwvjqiqpadqrkbdbgmxltowsulsifjeytrvadodlrqjqkqmhdfzjnycmhamnhnvoestquzqsdigkqutggfvmmnmmbcljqpdqretrgiqbaegkfvwcoepsmquveegoeflblylpbxeqqalpnuzbejrtbzftiaamblqbnmgfntyeygdobjwzlkxgvjbtfevovxpdhtobynfjpbwzpyblxgnngabhdxsozyfbvyptngbulzbyfhzmmorsvtjzwasysfnbnhotogetzkxfmtmrxeeijgkmckgsdazllkdygfakmcggghbrofvccglkhtbfqkvxjqbomxzkrlpztostdngxubecdftpovtpelbanmrligorzwmtyyetmzkiaywrvgrxgbyxiexbbmrygmgxqyzksaptzmacbcbkmwpczwbcectqsrivakpfoltlggmvhrktktrhhmjfrxmizedlxcwshictcorkbauclazedbiuvnrdssyhjhhkozopzeyutmioyxpbozhdmgsudrbtfjexdkkbbydwefkaxiqdlijunknzphphusqjwrprvfnprmqjywfrmfiskrwhsybpuhqhnqdicjvrrizmxhqrgffhrflbfytcpuaoucbnfwccjciwyunkdfltyqijlxamnyeujryjdvokmtnzfxnbczgzvtgzfnxmmzomsycoqjaugdotxmbrqxzalnjvsrcxqybdyklbwaabtyrmzwvccfjhipljnompibjwqrinhgjvdrkbujlebrbbyvnrvhbgmllzuoiivibtlbeljsdbagadzruklkuplwwgmzuxjztjtcmzbijlrgqugcvykmodegokdtkuhazbtpkmeuupzduogfpdpwdyjnemincaagughdvgcipfopiehkxvfmgzshvnpbrlvnppxqedtvmdxefnbatubgycdwsdhrhznbalwnbmzelkiqbdqackazrwpkhschlemnndehtjvndsivuorbwhxcbchcdgxhvreuukajrofukpwacoigratsrzgopafcrgahpdlhmqaauhfimkxsdyeoqaddkgycealzyekiowewhhsszzzplydrvwxlazajhlqjtsihmqxtshyamphwjrhubhitvpboowssejuoshtuixyratqojwmefkabtpxymcazvqayrfxobdgzwhutobsmdnnnhrkrfpiciwqecbetnrblrkzjyfnkperecflhfwpjzpwpqkbljikmqlkcnqzpdsbdcohqfdtzobyxrrodhlexccuafabrjqvdrwibljyihanypettzkibzkzmitfoknfrhvbwvoqiopgojujkdofcyplbnzphfoqlsytuccqgsynrcmcxxopmmiyikukhzfhkhigsvgijuomaisgiyeafkghmqzfqdrjmajxlijnpyomactuedqvtphkqneuehjjrphlubagoqtmxpueoivnulqosttvjmotnvydatuedcyualckhqzppvkstwljmedkqirpfruhhruqtgetmkucexxodjvfelnuggzoypbqgndtiafyltsxuyfvxbgumbbpfmcuuecfoatmhxwnimtqznazvjspudbyyzledyqquqjezzygouvtxntlwxxvxxrcjvoaavrcfmoqbjmkrsdtufmeydckphpmkrwjnrmuqzdvocxwwmvxbbprjivhqoubvbobrugtsebfngqzefghnozfndbavajptynmmncwjtdjrxfsrqqczhjbxegtzrwkrpugdmtooxvjdxfpzykabhtjegsexcxgiiopswinyiiewlttuhxwobixddbxqnygpjviclbtrtgfvvybrabxspijpqopdupgzfwbjaoqdizahtkpfcdyswaqfxsenwmdjlkceezryoprexjjnjgtruzydrqevxurciyjvoesrvsgfdwbgcwbjagdnmbtpecgkciyfqvtexcttcphbaswiokcblklxdnzlxeynzikpsnwqpcaeopnkhrzzkjaqmetclkkpdbwevxlmrmztlzidwqumqqbubovejjcuaqihnpaoflfojdmlighsiwpjveapgzhmectgeelijzorfnoclrmyzqowmbcaxpxpzvtijmjmthdndzrxmzonfdyiloponucsjcupjdllrqsgpdipeuwnaxoqvhvbvwztjbwlvttqravzoptmzfmiwltutlajvvaoupgmcuaqegldzblbziklwalayaxnzrtctjizyagbdccvumilwerfisqpjzgrdoupflvcazyzvezpnwytcdquykwyqqqtnoxxhlapwwcfjxmgwezbozvzkpjwwsypchuhxockjwbtnxbugyxrocaaofaijrogaktftkwpluufxddtynddjuletflhvtmxsfgwurtglkmhlklmfcsuvmdttiarycemxjlerikpiwjpixzrxukysxqzbssvzrvyhbnywmptxmjzyolsyexjbxdzmuljzvbofmuslrkfxrwuayrmmqhvgykhefcowknjcvuufbulrzjepnwvrfxpxhnlbmjthomjmnjluguikazdkrtxtvvhsfbpqrmlxsxoiwcqbpdpaurmrmlyaebiybvzoroyrntgwzmyadgcorwkqasjsstkixgdphwktyecetnkkzexnypjvjrrfscdtldxxpqdprdjsiyfkarxaektjxyezkxxwnigxtxveszwcxbeoznevqbhfzlqklamqhmjberqwzfsizcsejqaoihpfzqrmltlwtegzrxjjhprwmnujnpjtootpkatriftkarcldoflqfjmuycxkjdlkqughjzpolilftwbmcqvtnepwottvhrdyjvfsgdxrapextiqasxykpsuksmeotbbxzawpgzinexoskjivywchmjadujblhiioebgnubgitrrxjfdqivrprizowrgcsjlftecaaktywqzhrenjfezeydpbzeslxwrbvotqqzqafwroiaxuqgueoupyjkumhpbybuywzacbkxwxqgtjrsejhzsxqpveoqkbaljjsxcivkqcrhovpqiqhffmbwwgxyongndojyaamavpytmsyfbmuaepwpmhckcawgehqtmxzbecerqkeufiyvxpmvwtynbeyjpcrdpoptkdybmrrqzswrwwzwmdyzhfedhxyjahtcywhbvwrvrjnjwbxbwyyonvvguinkkihgbxmlmadbfgevdndvtpqylfoupyuifrqbtuacjgmtrvyysffndriwocztduufraszswkdrqylbjemoqyqeyzxfbweyciopxoqbcokokyrfgdygivtbfscfxhhjhrmfygfobauuwfynaecrqtdcdrpivrgleywoqbhgzuigkzojpysexwuounipsbonlbfzjkfzfhexlqivqiqhemkhcwsllxzoewnmelvqogpfntcahzmzpbkbpvtuduijnxwcevxdxjqtvhjpxfrqoaqlmkbzummucddhkbflpujhsbmsnrwmvmphrrgvabpvbopkxhtpueuuisubxmogpvrtixyhgfmmzeljhcjvculfcgekgcdwgsdylhbrkypwuucmscmjcooehesgokodlayriynufmblrslrxjkargfvjigbrpdkhaihyuphixzajhrzgujdemfchwxvuppqpbszhbrozdaakjahbzyojmvbsergrhaidlmilyamwcuwwdefblbljnweemsjhzeskrjczxmvrdieoamaepuxpwptydrfotcpogrhcgcukudopimxyjhzvoudltceuplcpftyznzoubvclzflyarvecggeccdylfbmoohrxhikunrqjwcddbspjbgzakaejrswdsmyrpstzbvtyhednoddsukiiesmmsyhzrxvszwzaodjlnneqpemkmnbdtvuwfhycifksoybyyvodcputbudsaarqkbhmpjturytkqktybjpqdcglokwyvqmhprlzygrngwlfquojghlkotmsejqbppqvafjwnsoeuwslmpwtszgommrcrqwpqyxrivypphcwwictwbwvehvvdkzikckqiyltuhlzwbcfpegxwpawajqenekrzajzdhgyckdbkvakwkodhqidmtitqufutygtjkdnfdllaonsdbbqibntvqedjzqczpowtvltrpmjzljwpxrmgcstqdnicimnbxbilbnqzwietifhucvdvtypzvypaxiqspbptpuoojxygkdynhglxqejjoiebtauquqmggtjohouazxqvnwlytmaunfjghnrelsugsomwkgsizjnwahztpkwrgqpysypqgrwkptzhawnjzisgkwmosguslernhgjfnuamtylwnvqxzauohojtggmququatbeiojjeqxlghnydkgyxjoouptpbpsqixapyvzpytvdvcuhfiteiwzqnblibxbnmicindqtscgmrxpwjlzjmprtlvtwopzcqzjdeqvtnbiqbbdsnoalldfndkjtgytufuqtitmdiqhdokwkavkbdkcyghdzjazrkeneqjawapwxgepfcbwzlhutlyiqkckizkdvvhevwbwtciwwchppyvirxyqpwqrcrmmogzstwpmlswueosnwjfavqppbqjesmtoklhgjouqflwgnrgyzlrphmqvywkolgcdqpjbytkqktyrutjpmhbkqraasdubtupcdovyybyoskficyhfwuvtdbnmkmepqennljdoazwzsvxrzhysmmseiikusddondehytvbztsprymsdwsrjeakazgbjpsbddcwjqrnukihxrhoombflydcceggcevraylfzlcvbuoznzytfpclpuectlduovzhjyxmipodukucgchrgopctofrdytpwpxupeamaoeidrvmxzcjrksezhjsmeewnjlblbfedwwucwmaylimldiahrgresbvmjoyzbhajkaadzorbhzsbpqppuvxwhcfmedjugzrhjazxihpuyhiahkdprbgijvfgrakjxrlsrlbmfunyiryaldokogseheoocjmcsmcuuwpykrbhlydsgwdcgkegcflucvjchjlezmmfghyxitrvpgomxbusiuueupthxkpobvpbavgrrhpmvmwrnsmbshjuplfbkhddcummuzbkmlqaoqrfxpjhvtqjxdxvecwxnjiudutvpbkbpzmzhactnfpgoqvlemnweozxllswchkmehqiqviqlxehfzfkjzfblnobspinuouwxesypjozkgiuzghbqowyelgrviprdcdtqrceanyfwuuabofgyfmrhjhhxfcsfbtvigydgfrykokocbqoxpoicyewbfxzyeqyqomejblyqrdkwszsarfuudtzcowirdnffsyyvrtmgjcautbqrfiuypuoflyqptvdndvegfbdamlmxbghikkniugvvnoyywbxbwjnjrvrwvbhwycthajyxhdefhzydmwzwwrwszqrrmbydktpopdrcpjyebnytwvmpxvyifuekqrecebzxmtqhegwackchmpwpeaumbfysmtypvamaayjodngnoyxgwwbmffhqiqpvohrcqkvicxsjjlabkqoevpqxszhjesrjtgqxwxkbcazwyubybphmukjypuoeugquxaiorwfaqzqqtovbrwxlsezbpdyezefjnerhzqwytkaacetfljscgrwozirprviqdfjxrrtigbungbeoiihlbjudajmhcwyvijksoxenizgpwazxbbtoemskuspkyxsaqitxeparxdgsfvjydrhvttowpentvqcmbwtflilopzjhguqkldjkxcyumjfqlfodlcraktfirtakptootjpnjunmwrphjjxrzgetwltlmrqzfphioaqjesczisfzwqrebjmhqmalkqlzfhbqvenzoebxcwzsevxtxginwxxkzeyxjtkeaxrakfyisjdrpdqpxxdltdcsfrrjvjpynxezkknteceytkwhpdgxiktssjsaqkwrocgdaymzwgtnryorozvbyibeaylmrmruapdpbqcwioxsxlmrqpbfshvvtxtrkdzakiuguljnmjmohtjmblnhxpxfrvwnpejzrlubfuuvcjnkwocfehkygvhqmmryauwrxfkrlsumfobvzjlumzdxbjxeysloyzjmxtpmwynbhyvrzvssbzqxsykuxrzxipjwipkireljxmecyraittdmvuscfmlklhmklgtruwgfsxmtvhlftelujddnytddxfuulpwktftkagorjiafoaacorxygubxntbwjkcoxhuhcpyswwjpkzvzobzewgmxjfcwwpalhxxontqqqywkyuqdctywnpzevzyzacvlfpuodrgzjpqsifrewlimuvccdbgayzijtctrznxayalawlkizblbzdlgeqaucmgpuoavvjaltutlwimfzmtpozvarqttvlwbjtzwvbvhvqoxanwuepidpgsqrlldjpucjscunopoliydfnozmxrzdndhtmjmjitvzpxpxacbmwoqzymrlconfrozjileegtcemhzgpaevjpwishgilmdjoflfoapnhiqaucjjevobubqqmuqwdizltzmrmlxvewbdpkklctemqajkzzrhknpoeacpqwnspkiznyexlzndxlklbckoiwsabhpcttcxetvqfyickgceptbmndgajbwcgbwdfgsvrseovjyicruxveqrdyzurtgjnjjxerpoyrzeeckljdmwnesxfqawsydcfpkthazidqoajbwfzgpudpoqpjipsxbarbyvvfgtrtblcivjpgynqxbddxibowxhuttlweiiyniwspoiigxcxesgejthbakyzpfxdjvxootmdguprkwrztgexbjhzcqqrsfxrjdtjwcnmmnytpjavabdnfzonhgfezqgnfbestgurbobvbuoqhvijrpbbxvmwwxcovdzqumrnjwrkmphpkcdyemfutdsrkmjbqomfcrvaaovjcrxxvxxwltnxtvuogyzzejquqqydelzyybdupsjvzanzqtminwxhmtaofceuucmfpbbmugbxvfyuxstlyfaitdngqbpyozggunlefvjdoxxecukmtegtqurhhurfpriqkdemjlwtskvppzqhkclauycdeutadyvntomjvttsoqlunvioeupxmtqogabulhprjjheuenqkhptvqdeutcamoypnjilxjamjrdqfzqmhgkfaeyigsiamoujigvsgihkhfzhkukiyimmpoxxcmcrnysgqccutyslqofhpznblpycfodkjujogpoiqovwbvhrfnkoftimzkzbikzttepynahiyjlbiwrdvqjrbafauccxelhdorrxyboztdfqhocdbsdpzqncklqmkijlbkqpwpzjpwfhlfcerepknfyjzkrlbrntebceqwicipfrkrhnnndmsbotuhwzgdboxfryaqvzacmyxptbakfemwjoqtaryxiuthsoujesswoobpvtihbuhrjwhpmayhstxqmhistjqlhjazalxwvrdylpzzzsshhwewoikeyzlaecygkddaqoeydsxkmifhuaaqmhldphagrcfapogzrstargiocawpkuforjakuuervhxgdchcbcxhwbrouvisdnvjthednnmelhcshkpwrzakcaqdbqiklezmbnwlabnzhrhdswdcygbutabnfexdmvtdeqxppnvlrbpnvhszgmfvxkheipofpicgvdhgugaacnimenjydwpdpfgoudzpuuemkptbzahuktdkogedomkyvcguqgrljibzmctjtzjxuzmgwwlpuklkurzdagabdsjlebltbiviiouzllmgbhvrnvybbrbeljubkrdvjghnirqwjbipmonjlpihjfccvwzmrytbaawblkydbyqxcrsvjnlazxqrbmxtodguajqocysmozmmxnfzgtvzgzcbnxfzntmkovdjyrjueynmaxljiqytlfdknuywicjccwfnbcuoaupctyfblfrhffgrqhxmzirrvjcidqnhqhupbyshwrksifmrfwyjqmrpnfvrprwjqsuhphpznknujildqixakfewdybbkkdxejftbrdusgmdhzobpxyoimtuyezpozokhhjhyssdrnvuibdezalcuabkroctcihswcxldezimxrfjmhhrtktkrhvmggltlofpkavirsqtcecbwzcpwmkbcbcamztpaskzyqxgmgyrmbbxeixybgxrgvrwyaikzmteyytmwzrogilrmnableptvoptfdcebuxgndtsotzplrkzxmobqjxvkqfbthklgccvforbhgggcmkafgydkllzadsgkcmkgjieexrmtmfxkztegotohnbnfsysawzjtvsrommzhfybzlubgntpyvbfyzosxdhbagnngxlbypzwbpjfnybothdpxvoveftbjvgxklzwjbodgyeytnfgmnbqlbmaaitfzbtrjebzunplaqqexbplylblfeogeevuqmspeocwvfkgeabqigrterqdpqjlcbmmnmmvfggtuqkgidsqzuqtseovnhnmahmcynjzfdhmqkqjqrldodavrtyejfisluswotlxmgbdbkrqdapqiqjvwptdnvnpnhrqslidyflmgriezjxjizxdmkpotnittvlpljtqhvfpgqyyhoqfgxzzuuptzcsnuhorcvupeoxuernhimkgoebkkbzpkkzbglenmhdxwirxdvpbcxkrzehnetibqzbyqjrjhlooaonbkaylbnbyokjazkarczqofifqvmbkdlumawpndwqjgdxqoineyvuzvhdyokipcsysaxbzjwscvtthqjylybizxcyzrdvcvriksbmaxqfibcntsodysuaoipfrgqperywylovhzpzotzaeislvhugaxgkxizukygjhfdxmzdmtpnfinyecmwfvsyaoyzpcysvpdqfhpkhpzoavhmtmmdyqzlxbcroemooqkkwbokecvzooydivbvbycgrzaiwuwxfmcmmlmyuegxdyrsrteltdddetfocyxlmstobbvsuyvjzlriqccgyaayumyoszgfurenxzefexvdyvrowanjhnbwpszwgwepuyajvfhfvfmciejrzrssaudvqqworofkfwcpxcsgqeqwwistuijyiplictssmhroeafvwtnttgsvpoagdgwtsngrkewzyxnzbkecwezeydkyiptvhthoimsqnvrtreivgafxqriagtbhwjgvjhtmvu"));
+    }
+
+    public static int countConsistentStrings(String allowed, String[] words) {
+        int count = words.length - 1;
+        for (int i = 0; i < count; i++) {
+            for (int w = 0; w < allowed.length(); w++) {
+                if (words[i].indexOf(allowed.charAt(w)) == -1) {
+                    count--;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> respond = new ArrayList<>();
+        int max = Arrays.stream(candies).max().getAsInt();
+        for (int i = 0; i < candies.length; i++) {
+            if (candies[i] + extraCandies >= max) {
+                respond.add(true);
+            } else {
+                respond.add(false);
+            }
+        }
+        return respond;
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        int s = nums.length;
+        int j = 1;
+        for (int i = 1; i < s; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+
+    public static boolean isPalindrome(String s) {
+        s = s.replaceAll("[^a-zA-Z]", "").toLowerCase();
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean validPalindrome(String s) {
+
+
+        for (int i = 0; i < s.length(); i++) {
+            StringBuilder sb = new StringBuilder(s);
+            sb.deleteCharAt(i);
+            if (isPalindrome(sb.toString())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static String reverseWords(String s) {
+        String words[] = s.split(" ");
+        String reversedWords = "";
+        for (String word : words) {
+            reversedWords += new StringBuilder(word).reverse() + " ";
+        }
+        return reversedWords.trim();
+    }
+
+    public boolean isMatches(String[] words, String query) {
+        char[] queryChars = query.toCharArray();
+        for (int j = 0; j < words.length; j++) {
+            char[] wordChars = words[j].toCharArray();
+            if (queryChars.length == wordChars.length) {
+                for (int i = 0; i < wordChars.length; i++) {
+                    if (queryChars[i] != '*' || queryChars[i] != wordChars[i]) {
+                        break;
+                    }
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+}
